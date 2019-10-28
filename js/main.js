9a00ad7562a104ef9f88f22c1323a3fe496e5607
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2019-10-27 19:08:13 -0500

    added generator

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
 //-------------constants-------------------
-
+const game = {
+    title:'Guess the number!',
+    max: 100, //max numer in range
+    min: 1, //min number
+    secretNum: null; //sec num
+};
 
 
 
 //-------------variables-------------------
-
+var prevGuesses = [];
 
 
 
 
 //-------------functions-------------------
+functions getGuess() {
+    game.secret = Math.floor(Math.random()*(game.max - game.min +1)) + game.min;
+    let guess = NaN;
+    while(guess !== game.secretNum) {
+        guess = getGuess();
+        prevGuesses.push(guess);
+        render(guess);
+        if (guess === game.secretNum) return;
+    }
+};
+
+functions play() {
+
+};
+
+functions render(guess) {
+
+};
+
